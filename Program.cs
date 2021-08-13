859959fe796ea2c40e2ed9f73f62edf668c0eb10
GitDogBar <Dogbar>
2021-08-13 14:28:59 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Geam_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //player 2 คนหัวใจ มีคนล่ะ 3 ดวง
+            //gramManager ทำหน้าที่คุมเกม
+        }
+    }
+}
